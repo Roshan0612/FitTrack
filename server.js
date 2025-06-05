0dd2ed590e9ad6e590b0a027dbaeed8eb775930a
Roshan Gawade <roshangawade160@gmail.com>
2025-06-05 16:51:12 +0530

    server change

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-gfdfivmifv
+const express =require("express");
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.listen(PORT,()=>{
+    console.log(`server runnih o port ${PORT}`);
+})
