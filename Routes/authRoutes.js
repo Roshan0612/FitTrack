f341effc75bb0ca28031ebb70d85b69738756a27
Roshan Gawade <roshangawade160@gmail.com>
2025-06-06 22:01:00 +0530

    4

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express =require("express");
-const {registerController,loginController} =require("../Controller/authController");
+const {registerController,loginController, demoofSignIN} =require("../Controller/authController");
+const { isAdmin, requireSignIn } = require("../Middleware.js/middleware");
 
 const router=express.Router();
 
@@ -7,5 +8,5 @@ const router=express.Router();
 
 router.post("/register",registerController);
 router.post("/login",loginController);
-
+router.get("/test", requireSignIn, isAdmin, demoofSignIN);
 module.exports=router;
