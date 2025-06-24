1b5e4b6b1eaaf82141a68672c7ed03a359f3327b
Roshan Gawade <roshangawade160@gmail.com>
2025-06-24 11:50:51 +0530

    38

--- backend/Model/userModel.js
+++ backend/Model/userModel.js
@@ -48,4 +48,4 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 
-module.exports = mongoose.models.User || mongoose.model("User", userSchema);
+module.exports =  mongoose.model("User", userSchema) || mongoose.models.User;
